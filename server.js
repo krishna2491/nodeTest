98e688894b7dfbfaacd3825c79cebd19aba94cf1
Krishnakant Bairagi <krishnakant.bairagi@yash.com>
2019-05-28 16:05:24 +0530

    testing purpose

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+var rest=require('./test.js');
+var hotel=require('./hotles.js');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
 	
-  res.send('Hello from App Engine!');
+  res.send('Hello from App Engine!kk');
 });
 
 app.post('/sum', (req, res) => {
 	
-  res.send('kk11');
+  res.send('kk11'+rest.getRestaurents());
+});
+app.get('/restaurents', (req, res) => {
+	
+  res.send(rest.getRestaurents());
+});
+app.get('/hotels', (req, res) => {
+	
+  res.send(hotel.getHotles());
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
