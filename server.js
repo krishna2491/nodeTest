6ac3b2f22f8409521c652459ed72d018844aa62a
Krishnakant Bairagi <krishnakant.bairagi@yash.com>
2019-05-29 14:34:13 +0530

    cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var hotel=require('./hotles.js');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
